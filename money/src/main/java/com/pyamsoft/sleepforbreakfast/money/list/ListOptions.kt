1f5534dac63c0acf365603e4b461903223b05b21
pyamsoft <developer.pyamsoft@gmail.com>
2023-04-03 13:20:15 -0700

    Debouce search
    So that we don't recreate the list every keypress

--- money/src/main/java/com/pyamsoft/sleepforbreakfast/money/list/ListOptions.kt
+++ money/src/main/java/com/pyamsoft/sleepforbreakfast/money/list/ListOptions.kt
@@ -22,14 +22,18 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
 import com.pyamsoft.pydroid.theme.keylines
 import com.pyamsoft.pydroid.ui.defaults.CardDefaults
+import kotlinx.coroutines.delay
 
 @Composable
 fun Search(
@@ -61,8 +65,16 @@ fun SearchBar(
     onSearchToggled: () -> Unit,
     onSearchUpdated: (String) -> Unit,
 ) {
-  val search by state.search.collectAsState()
+  val initialSearch by state.search.collectAsState()
   val isOpen by state.isSearchOpen.collectAsState()
+
+  // Do this so that we can debounce typing events
+  val (search, setSearch) = remember { mutableStateOf(initialSearch) }
+  val handleSearchUpdated by rememberUpdatedState(onSearchUpdated)
+  LaunchedEffect(search) {
+    delay(300L)
+    handleSearchUpdated(search)
+  }
 
   BackHandler(
       enabled = isOpen,
@@ -85,7 +97,7 @@ fun SearchBar(
         TextField(
             modifier = Modifier.fillMaxWidth(),
             value = search,
-            onValueChange = onSearchUpdated,
+            onValueChange = { setSearch(it) },
             keyboardOptions =
                 remember {
                   KeyboardOptions(
@@ -101,7 +113,7 @@ fun SearchBar(
             },
             trailingIcon = {
               Icon(
-                  modifier = Modifier.clickable { onSearchUpdated("") },
+                  modifier = Modifier.clickable { setSearch("") },
                   imageVector = Icons.Filled.Clear,
                   contentDescription = "Clear",
               )
