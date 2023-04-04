9441674b45ff25a2edd686371823c1430c8367e4
pyamsoft <developer.pyamsoft@gmail.com>
2023-04-04 11:51:58 -0700

    More work on debounce

--- ui/src/main/java/com/pyamsoft/sleepforbreakfast/ui/Effects.kt
+++ ui/src/main/java/com/pyamsoft/sleepforbreakfast/ui/Effects.kt
@@ -1,6 +1,7 @@
 package com.pyamsoft.sleepforbreakfast.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -34,12 +35,26 @@ fun debouncedOnTextChange(
   val ret = remember { mutableStateOf(value) }
   val current = ret.value
 
+  // Each time the current changes, delay by a set amount and then fire the change out
+  // effectively deboucning changes while keeping track of the text
   LaunchedEffect(
       current,
       delay,
   ) {
     delay(delay)
     handleChange(current)
+  }
+
+  // When this Composable leaves the render pipe, we fire off a final change event
+  // To make sure that outside callers know about the most recent value
+  val mostRecentValue by rememberUpdatedState(current)
+  DisposableEffect(Unit) {
+    onDispose {
+      // Both of these values are remembered
+      // This way the disposable only mounts once, and then
+      // upon it finally leaving, we fire the most up-to-date value out
+      handleChange(mostRecentValue)
+    }
   }
 
   return ret
