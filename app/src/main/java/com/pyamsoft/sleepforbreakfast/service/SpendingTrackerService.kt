6f302b7a353deab444f7934177e5790be60f0c44
pyamsoft <developer.pyamsoft@gmail.com>
2023-04-05 21:13:15 -0700

    Clean up notification service to avoid leak

--- app/src/main/java/com/pyamsoft/sleepforbreakfast/service/SpendingTrackerService.kt
+++ app/src/main/java/com/pyamsoft/sleepforbreakfast/service/SpendingTrackerService.kt
@@ -33,39 +33,23 @@ class SpendingTrackerService : NotificationListenerService() {
 
   @Inject @JvmField internal var handler: SpendingTrackerHandler? = null
 
-  private fun withScope(block: CoroutineScope.() -> Unit) {
-    scope?.also(block)
-  }
-
-  override fun onListenerConnected() {
-    Timber.d("Service connected!")
-
-    scope?.cancel()
-    scope = MainScope()
-  }
-
-  override fun onListenerDisconnected() {
-    Timber.d("Service disconnected!")
-
-    scope?.cancel()
-    scope = null
-
-    handler = null
-  }
-
-  override fun onNotificationPosted(sbn: StatusBarNotification) {
-    withScope { launch(context = Dispatchers.IO) { processNotification(sbn) } }
-  }
-
   private fun ensureInjected() {
     if (handler == null) {
       ObjectGraph.ApplicationScope.retrieve(application).inject(this)
     }
   }
 
-  private suspend fun processNotification(sbn: StatusBarNotification) {
-    ensureInjected()
+  override fun onListenerConnected() {
+    scope = scope ?: MainScope()
+  }
 
+  override fun onDestroy() {
+    scope?.cancel()
+    scope = null
+    handler = null
+  }
+
+  override fun onNotificationPosted(sbn: StatusBarNotification) {
     val notif = sbn.notification
     if (notif == null) {
       Timber.w("SBN is missing notification data")
@@ -78,6 +62,7 @@ class SpendingTrackerService : NotificationListenerService() {
       return
     }
 
-    handler?.processNotification(sbn, extras)
+    ensureInjected()
+    scope?.apply { launch(context = Dispatchers.IO) { handler?.processNotification(sbn, extras) } }
   }
 }
