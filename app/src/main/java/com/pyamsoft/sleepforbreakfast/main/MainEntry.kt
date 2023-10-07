7789d77f7d381aa601a715b13f893b2863780079
pyamsoft <developer.pyamsoft@gmail.com>
2023-10-07 15:11:55 -0700

    Apply over the Main

--- app/src/main/java/com/pyamsoft/sleepforbreakfast/main/MainEntry.kt
+++ app/src/main/java/com/pyamsoft/sleepforbreakfast/main/MainEntry.kt
@@ -17,7 +17,9 @@
 package com.pyamsoft.sleepforbreakfast.main
 
 import androidx.activity.ComponentActivity
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -29,6 +31,7 @@ import com.pyamsoft.pydroid.ui.inject.rememberComposableInjector
 import com.pyamsoft.pydroid.ui.theme.Theming
 import com.pyamsoft.pydroid.ui.util.rememberNotNull
 import com.pyamsoft.sleepforbreakfast.ObjectGraph
+import com.pyamsoft.sleepforbreakfast.money.LocalCategoryColor
 import com.pyamsoft.sleepforbreakfast.money.category.CategoryIdMapper
 import javax.inject.Inject
 
@@ -78,17 +81,21 @@ internal fun MainEntry(
       mapper = mapper,
   )
 
-  MainScreen(
-      modifier = modifier,
-      appName = appName,
-      state = viewModel,
-      mapper = mapper,
-      onOpenSettings = { viewModel.handleOpenSettings() },
-      onCloseSettings = { viewModel.handleCloseSettings() },
-      onClosePage = { viewModel.handleClosePage() },
-      onOpenTransactions = { viewModel.handleOpenTransactions(it) },
-      onOpenAllTransactions = { viewModel.handleOpenAllTransactions() },
-      onOpenRepeats = { viewModel.handleOpenRepeats() },
-      onOpenCategories = { viewModel.handleOpenCategory() },
-  )
+  CompositionLocalProvider(
+      LocalCategoryColor provides MaterialTheme.colors.primary,
+  ) {
+    MainScreen(
+        modifier = modifier,
+        appName = appName,
+        state = viewModel,
+        mapper = mapper,
+        onOpenSettings = { viewModel.handleOpenSettings() },
+        onCloseSettings = { viewModel.handleCloseSettings() },
+        onClosePage = { viewModel.handleClosePage() },
+        onOpenTransactions = { viewModel.handleOpenTransactions(it) },
+        onOpenAllTransactions = { viewModel.handleOpenAllTransactions() },
+        onOpenRepeats = { viewModel.handleOpenRepeats() },
+        onOpenCategories = { viewModel.handleOpenCategory() },
+    )
+  }
 }
--- app/src/main/java/com/pyamsoft/sleepforbreakfast/repeat/add/RepeatAddEntry.kt
+++ app/src/main/java/com/pyamsoft/sleepforbreakfast/repeat/add/RepeatAddEntry.kt
@@ -17,9 +17,7 @@
 package com.pyamsoft.sleepforbreakfast.repeat.add
 
 import androidx.activity.ComponentActivity
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
@@ -28,7 +26,6 @@ import com.pyamsoft.pydroid.ui.inject.ComposableInjector
 import com.pyamsoft.pydroid.ui.inject.rememberComposableInjector
 import com.pyamsoft.pydroid.ui.util.rememberNotNull
 import com.pyamsoft.sleepforbreakfast.ObjectGraph
-import com.pyamsoft.sleepforbreakfast.money.LocalCategoryColor
 import com.pyamsoft.sleepforbreakfast.money.category.CategoryIdMapper
 import com.pyamsoft.sleepforbreakfast.ui.SurfaceDialog
 import javax.inject.Inject
@@ -87,35 +84,31 @@ internal fun RepeatAddEntry(
       viewModel = viewModel,
   )
 
-  CompositionLocalProvider(
-      LocalCategoryColor provides MaterialTheme.colors.primary,
+  SurfaceDialog(
+      modifier = modifier,
+      onDismiss = onDismiss,
   ) {
-    SurfaceDialog(
-        modifier = modifier,
+    RepeatAddScreen(
+        state = viewModel,
+        mapper = mapper,
         onDismiss = onDismiss,
-    ) {
-      RepeatAddScreen(
-          state = viewModel,
-          mapper = mapper,
-          onDismiss = onDismiss,
-          onNameChanged = { viewModel.handleNameChanged(it) },
-          onNoteChanged = { viewModel.handleNoteChanged(it) },
-          onAmountChanged = { viewModel.handleAmountChanged(it) },
-          onTypeChanged = { viewModel.handleTypeChanged(it) },
-          onCategoryAdded = { viewModel.handleCategoryAdded(it) },
-          onCategoryRemoved = { viewModel.handleCategoryRemoved(it) },
-          onDateChanged = { viewModel.handleDateChanged(it) },
-          onOpenDateDialog = { viewModel.handleOpenDateDialog() },
-          onCloseDateDialog = { viewModel.handleCloseDateDialog() },
-          onRepeatTypeChanged = { viewModel.handleRepeatTypeChanged(it) },
-          onReset = { viewModel.handleReset() },
-          onSubmit = {
-            viewModel.handleSubmit(
-                scope = scope,
-                onDismissAfterUpdated = onDismiss,
-            )
-          },
-      )
-    }
+        onNameChanged = { viewModel.handleNameChanged(it) },
+        onNoteChanged = { viewModel.handleNoteChanged(it) },
+        onAmountChanged = { viewModel.handleAmountChanged(it) },
+        onTypeChanged = { viewModel.handleTypeChanged(it) },
+        onCategoryAdded = { viewModel.handleCategoryAdded(it) },
+        onCategoryRemoved = { viewModel.handleCategoryRemoved(it) },
+        onDateChanged = { viewModel.handleDateChanged(it) },
+        onOpenDateDialog = { viewModel.handleOpenDateDialog() },
+        onCloseDateDialog = { viewModel.handleCloseDateDialog() },
+        onRepeatTypeChanged = { viewModel.handleRepeatTypeChanged(it) },
+        onReset = { viewModel.handleReset() },
+        onSubmit = {
+          viewModel.handleSubmit(
+              scope = scope,
+              onDismissAfterUpdated = onDismiss,
+          )
+        },
+    )
   }
 }
