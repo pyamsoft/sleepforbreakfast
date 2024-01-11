32a865837db3de9b2d130bab8f485d4e7e6e4c56
pyamsoft <developer.pyamsoft@gmail.com>
2024-01-11 13:52:11 -0800

    Drop FGS permission pulled from Workmanager

--- automatic/src/main/java/com/pyamsoft/sleepforbreakfast/automatic/add/AutomaticAddViewModeler.kt
+++ automatic/src/main/java/com/pyamsoft/sleepforbreakfast/automatic/add/AutomaticAddViewModeler.kt
@@ -27,13 +27,13 @@ import com.pyamsoft.sleepforbreakfast.db.notification.DbNotificationWithRegexes
 import com.pyamsoft.sleepforbreakfast.db.transaction.DbTransaction
 import com.pyamsoft.sleepforbreakfast.money.one.OneViewModeler
 import java.time.Clock
+import java.time.LocalDateTime
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.time.LocalDateTime
 
 class AutomaticAddViewModeler
 @Inject
