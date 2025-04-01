db8b11a5866e20567b0e6001825cf1eb2f4af44f
pyamsoft <developer.pyamsoft@gmail.com>
2025-03-31 21:17:46 -0700

    Move to DataStore

--- app/src/main/java/com/pyamsoft/sleepforbreakfast/preference/PreferencesImpl.kt
+++ app/src/main/java/com/pyamsoft/sleepforbreakfast/preference/PreferencesImpl.kt
@@ -17,21 +17,24 @@
 package com.pyamsoft.sleepforbreakfast.preference
 
 import android.content.Context
-import android.content.SharedPreferences
-import androidx.core.content.edit
+import androidx.datastore.preferences.SharedPreferencesMigration
+import androidx.datastore.preferences.core.MutablePreferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.preference.PreferenceManager
 import com.pyamsoft.pydroid.core.ThreadEnforcer
-import com.pyamsoft.pydroid.util.preferenceBooleanFlow
 import com.pyamsoft.sleepforbreakfast.db.DbPreferences
-import javax.inject.Inject
-import javax.inject.Singleton
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Singleton
 
 @Singleton
 internal class PreferencesImpl
@@ -41,31 +44,54 @@ internal constructor(
     private val enforcer: ThreadEnforcer,
 ) : DbPreferences {
 
-  private val preferences by lazy {
-    enforcer.assertOffMainThread()
-    PreferenceManager.getDefaultSharedPreferences(context.applicationContext)
-  }
-
-  private val scope by lazy {
-    CoroutineScope(
-        context = Dispatchers.IO + SupervisorJob() + CoroutineName(this::class.java.name),
+    private val Context.dataStore by
+    preferencesDataStore(
+        name = "sleepforbreakfast_preferences",
+        produceMigrations = {
+            listOf(
+                // NOTE(Peter): Since our shared preferences was the DEFAULT process one, loading up
+                //              a migration without specifying all keys will also migrate
+                //              PYDROID SPECIFIC PREFERENCES which is what we do NOT want to do.
+                //              We instead maintain ONLY a list of the known app preference keys
+                SharedPreferencesMigration(
+                    keysToMigrate =
+                        setOf(
+                            KEY_DEFAULT_CATEGORIES.name,
+                        ),
+                    produceSharedPreferences = {
+                        PreferenceManager.getDefaultSharedPreferences(context.applicationContext)
+                    },
+                ),
+            )
+        },
     )
-  }
 
-  private inline fun setPreference(crossinline block: suspend SharedPreferences.Editor.() -> Unit) {
-    scope.launch(context = Dispatchers.IO) { preferences.edit { block() } }
-  }
+    private val preferences by lazy {
+        enforcer.assertOffMainThread()
+        context.applicationContext.dataStore
+    }
 
-  override fun listenSystemCategoriesPreloaded(): Flow<Boolean> =
-      preferenceBooleanFlow(KEY_DEFAULT_CATEGORIES, false) { preferences }
-          .flowOn(context = Dispatchers.IO)
+    private val scope by lazy {
+        CoroutineScope(
+            context = Dispatchers.IO + SupervisorJob() + CoroutineName(this::class.java.name),
+        )
+    }
 
-  override fun markSystemCategoriesPreloaded() {
-    setPreference { putBoolean(KEY_DEFAULT_CATEGORIES, true) }
-  }
+    private inline fun setPreference(crossinline block: suspend (MutablePreferences) -> Unit) {
+        scope.launch(context = Dispatchers.IO) { preferences.edit { block(it) } }
+    }
 
-  companion object {
+    override fun listenSystemCategoriesPreloaded(): Flow<Boolean> =
+        preferences.data.map { it[KEY_DEFAULT_CATEGORIES] ?: DEFAULT_DEFAULT_CATEGORIES }
+            .flowOn(context = Dispatchers.IO)
 
-    private const val KEY_DEFAULT_CATEGORIES = "key_default_categories"
-  }
+    override fun markSystemCategoriesPreloaded() {
+        setPreference { it[KEY_DEFAULT_CATEGORIES] = true }
+    }
+
+    companion object {
+
+        private val KEY_DEFAULT_CATEGORIES = booleanPreferencesKey("key_default_categories")
+        private const val DEFAULT_DEFAULT_CATEGORIES = false
+    }
 }
