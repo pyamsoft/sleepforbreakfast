272fe7546e68de96367e5db3c440b91f127f9b40
pyamsoft <developer.pyamsoft@gmail.com>
2026-09-16 07:37:57 -0700

    Format

--- app/src/google/java/com/pyamsoft/sleepforbreakfast/pydroidProvider.kt
+++ app/src/google/java/com/pyamsoft/sleepforbreakfast/pydroidProvider.kt
@@ -18,8 +18,8 @@ package com.pyamsoft.sleepforbreakfast
 
 import androidx.annotation.CheckResult
 import com.pyamsoft.pydroid.billing.BillingModule
-import com.pyamsoft.pydroid.bootstrap.libraries.OssLibraries
 import com.pyamsoft.pydroid.billing.store.PlayBillingModule
+import com.pyamsoft.pydroid.bootstrap.libraries.OssLibraries
 import com.pyamsoft.pydroid.bootstrap.play.rating.PlayRatingModule
 import com.pyamsoft.pydroid.bootstrap.play.version.PlayVersionModule
 import com.pyamsoft.pydroid.bootstrap.rating.RatingModule
